67bf5464e7a1ca4f7c54f093418189f2e8e766fd
iulon <i.magularia357@gmail.com>
2023-07-27 21:51:19 +0400

    დავალება 2 დასასრულებელია

--- js/app.js
+++ js/app.js
@@ -12,3 +12,10 @@ btn.addEventListener('click', () => {
     
     magic.style.display = 'block';
 })
+
+////დავალება 2:
+
+const img = document.createElement("img");
+img.src = "https://fastly.picsum.photos/id/405/1400/800.jpg?hmac=4CRI7OpfDWtP4EydVd4Z-1NKSGhBmpZq8OaEXVu3be8";
+const src = document.getElementById("forPicture");
+src.appendChild(img);
