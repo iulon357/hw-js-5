c70a75bed919f5a5db9fc874f506cd2c65112cc6
iulon <i.magularia357@gmail.com>
2023-07-27 21:31:44 +0400

    დავალება 1 შესრულებულია

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+
+//დავალება 1:
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click', () => {
+    
+    btn.style.display = 'none';
+
+    const magic = document.getElementById("magic");
+    
+    magic.style.display = 'block';
+})
